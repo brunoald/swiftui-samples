026fed9cf6c6b710962e1939570a43ccb7e6f69b
Bruno Dias <brunoald@gmail.com>
2022-03-17 18:32:27 -0300

    Configura a ViewModel do StopwatchView

--- Stopwatch/StopwatchView.swift
+++ Stopwatch/StopwatchView.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 
 struct StopwatchView: View {
+    @StateObject
+    var viewModel = StopwatchViewModel()
+
+    @State
+    var counterValue: String = "00:00"
+
     var body: some View {
         VStack {
             counter
@@ -9,24 +15,27 @@ struct StopwatchView: View {
     }
 
     private var counter: some View {
-        Text("00:00")
+        Text(viewModel.counterValue)
             .font(.system(size: 120, weight: .black, design: .default))
     }
 
     private var buttons: some View {
         HStack {
+            if viewModel.isPlaying {
+                Button(action: {
+                    viewModel.pause()
+                }, label: {
+                    Image(systemName: "pause.circle.fill")
+                })
+            } else {
+                Button(action: {
+                    viewModel.play()
+                }, label: {
+                    Image(systemName: "play.circle.fill")
+                })
+            }
             Button(action: {
-                // Ação de Play
-            }, label: {
-                Image(systemName: "play.circle.fill")
-            })
-            Button(action: {
-                // Ação de Pause
-            }, label: {
-                Image(systemName: "pause.circle.fill")
-            })
-            Button(action: {
-                // Ação de Reiniciar
+                viewModel.reset()
             }, label: {
                 Image(systemName: "arrow.counterclockwise.circle.fill")
             })
--- Stopwatch/StopwatchViewModel.swift
+++ Stopwatch/StopwatchViewModel.swift
@@ -1,8 +1,42 @@
 import Foundation
+import Combine
 
-final class StopwatchViewModel {
-    func play() {}
-    func pause() {}
-    func reset() {}
+final class StopwatchViewModel: ObservableObject {
+
+    @Published
+    var counterValue: String = ""
+
+    var timer = Timer.publish(every: 1, tolerance: .none, on: RunLoop.main, in: .default)
+
+    @Published
+    var isPlaying = false
+
+    var secondsCount: Int = 0 {
+        didSet {
+            counterValue = "\(secondsCount)"
+        }
+    }
+
+    var cancellable: AnyCancellable? = nil
+
+    init() {
+        cancellable = timer.sink { _ in
+            if self.isPlaying {
+                self.secondsCount += 1
+            }
+        }
+        timer.connect()
+    }
+
+    func play() {
+        isPlaying = true
+    }
+    
+    func pause() {
+        isPlaying = false
+    }
+    func reset() {
+        secondsCount = 0
+    }
 }
 

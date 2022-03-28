02273e52e0da64cdb5ac795e8bb615b71e9119b6
Bruno Dias <brunoald@gmail.com>
2022-03-28 14:01:06 -0300

    Criação da estrutura inicial da lista de filmes

--- MovieDB/MoviesListView.swift
+++ MovieDB/MoviesListView.swift
@@ -6,9 +6,14 @@ struct MoviesList: View {
     @State var movies: [Movie] = []
 
     var body: some View {
-        VStack {
-            ForEach(movies, id: \.id) { movie in
-                Text(movie.title)
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(movies, id: \.id) { movie in
+                    MovieView(
+                        title: movie.title,
+                        posterUrl: movie.posterPath
+                    )
+                }
             }
         }.onAppear {
             api.getMovies { result in
@@ -20,6 +25,30 @@ struct MoviesList: View {
                 }
             }
         }
+    }
+}
+
+struct MovieView: View {
+    let title: String
+    let posterUrl: String
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string:"https://image.tmdb.org/t/p/original/\(posterUrl)")) { phase in
+
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } else if phase.error != nil {
+                    Color.red // Indicates an error.
+                } else {
+                    Color.blue // Acts as a placeholder.
+                }
+            }
+            .frame(width: 150, height: 200)
+            Text(title).bold().padding()
+        }
+        .padding()
     }
 }
 
